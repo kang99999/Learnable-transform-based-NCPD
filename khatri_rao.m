7ed3a64aaf38d2c70e645edd02d32493ffd089a8
kang99999 <140691681+kang99999@users.noreply.github.com>
2025-05-20 17:53:56 +0800

    Delete khatri_rao.m

--- khatri_rao.m
+++
@@ -1,8 +0,0 @@
-function kr = khatri_rao(F, G)
-    nR_F = size(F, 1); % F 的行数
-    nR_G = size(G, 1); % G 的行数
-    mul = ones(nR_G, 1);
-    FF = kron(F, mul); % 通过 kron 函数实现对 F 矩阵的扩充
-    GG = repmat(G, nR_F, 1); % 通过 repmat 函数实现对 G 矩阵的扩充
-    kr = FF .* GG; % Hadamard 积（逐元素乘积）
-end
